ed54d7a83890841d79b55ac3fa97c2caefb2f4bd
Lucas Täkker <lucastakker@gmail.com>
2026-02-14 15:02:56 +0100

    Added person card component for detail page

---
+++ app/components/Person/Card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const { person } = defineProps<{ person: Person }>()
+</script>
+
+<template>
+	<UCard
+		:ui="{ body: 'flex flex-col overflow-hidden gap-4 p-4' }"
+		class="transition-shadow hover:shadow-lg"
+	>
+		<div>
+			<h3 class="text-lg font-semibold">
+				{{ person.name }}
+			</h3>
+			<div class="text-sm flex gap-1.5 text-gray-500">
+				<p>
+					{{ person.gender }}
+				</p>
+				<p>-</p>
+				<p>{{ person.age }}</p>
+			</div>
+		</div>
+
+		<div class="text-xs text-gray-600 flex flex-wrap gap-2">
+			<span>Eyes: {{ person.eye_color }}</span>
+			<span>Hair: {{ person.hair_color }}</span>
+		</div>
+	</UCard>
+</template>
